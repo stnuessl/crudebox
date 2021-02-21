46da96660e2bf8bcbb81969fba5b810ee83b3270
Steffen Nuessle <nuessle.steffen@gmail.com>
2021-02-21 18:04:18 +0100

    Added 'errstr()' function and related bash script
    The function can be used to retrieve a simple human-readable error
    string for a given error code. This is useful when printing error
    messages and not having the urge to fiddle with 'strerror()'.
    
    An additional auxiliary script is provided to automatically generate
    the mapping table which is used for the error string lookup.

---
+++ scripts/mk-errstr.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+#
+# The MIT License (MIT)
+#
+# Copyright (c) 2021  Steffen Nuessle
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+
+# 
+# Use ":read !bash scripts/mk-errstr.sh" in vim 
+# to automatically create a designated array initializer list.
+#
+
+function main() {
+    local matches=$(grep \
+        --only-matching \
+        --no-filename \
+        -E "\bE[A-Z0-9]*\b\s+[0-9]+" \
+        /usr/include/asm-generic/errno*.h | awk '{ print $1 }');
+
+    printf "[0] = \"OK\"\n"
+
+    for str in ${matches}; do
+        printf "[${str}] = \"${str}\",\n"
+    done
+}
+
+main
